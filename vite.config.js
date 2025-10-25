6c3d6fc8b3dd97e53220fc8ed9377aea58382bcf
crazyjohann <jalengaden@stelizabetheg.org>
2025-10-24 19:07:01 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -22,5 +23,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  resolve: {
+    alias: {
+      '/src/assets': '/assets' // 👈 Maps your old asset paths to the new public/assets folder
+    }
   }
-})
+});
