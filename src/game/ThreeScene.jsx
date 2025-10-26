bd2552a692f7eb72432947dc785a36befbe9affb
crazyjohann <jalengaden@stelizabetheg.org>
2025-10-26 08:40:02 -0700

    Update ThreeScene.jsx

--- src/game/ThreeScene.jsx
+++ src/game/ThreeScene.jsx
@@ -1,18 +1,69 @@
-import React from 'react';
-import { OrbitControls } from '@react-three/drei';
-import Cars from './cars';
-import Tracks from './tracks';
+import React, { useEffect } from 'react';
+import { Sky } from '@react-three/drei';
+import { Car } from './cars';
+import { Track, GroundPlane } from './tracks';
 import CameraRig from './camera/CameraRig';
+import useInput from './input/useInput';
 
-export default function ThreeScene() {
+export default function ThreeScene({
+  selectedCar,
+  selectedTrack,
+  onCarRef,
+  carApi,
+  raceStarted,
+  cameraView,
+  setCameraView,
+  carRef
+}) {
+  const input = useInput();
+
+  useEffect(() => {
+    if (!carApi || !raceStarted) return;
+
+    const maxForce = 2000 * (selectedCar.acceleration / 100);
+    const maxTorque = 1.5 * (selectedCar.handling / 100);
+
+    if (input.forward) {
+      carApi.applyLocalForce([0, 0, -maxForce], [0, 0, 0]);
+    }
+    if (input.back) {
+      carApi.applyLocalForce([0, 0, maxForce * 0.6], [0, 0, 0]);
+    }
+    if (input.left) {
+      carApi.applyLocalTorque([0, maxTorque, 0]);
+    }
+    if (input.right) {
+      carApi.applyLocalTorque([0, -maxTorque, 0]);
+    }
+    if (input.boost) {
+      carApi.applyLocalForce([0, 0, -maxForce * 1.5], [0, 0, 0]);
+    }
+  }, [input, carApi, raceStarted, selectedCar]);
+
+  useEffect(() => {
+    if (input.camera) {
+      const views = ['chase', 'hood', 'cockpit', 'cinematic'];
+      const currentIndex = views.indexOf(cameraView);
+      const nextIndex = (currentIndex + 1) % views.length;
+      setCameraView(views[nextIndex]);
+    }
+  }, [input.camera, cameraView, setCameraView]);
+
   return (
     <>
-      {/* Optional orbit controls for debugging */}
-      <OrbitControls enabled={false} />
-      <ambientLight intensity={0.4} />
-      <Tracks />
-      <Cars />
-      <CameraRig />
+      <Sky sunPosition={[100, 20, 100]} />
+      
+      <Track trackData={selectedTrack} />
+      <GroundPlane />
+      
+      <Car
+        carData={selectedCar}
+        position={[0, 1, 0]}
+        isPlayer
+        onCarRef={onCarRef}
+      />
+      
+      {carRef && <CameraRig target={carRef} cameraView={cameraView} />}
     </>
   );
 }
